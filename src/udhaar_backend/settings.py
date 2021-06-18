9e8733cda884146664fbc5fbf91177251bbd5588
Divya Garg <divyagarg2601@gmail.com>
2021-06-18 06:16:17 +0530

    Change page size in pagination

--- src/udhaar_backend/settings.py
+++ src/udhaar_backend/settings.py
@@ -60,7 +60,7 @@ SWAGGER_SETTINGS = {
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 30,
 }
 
 MIDDLEWARE = [
