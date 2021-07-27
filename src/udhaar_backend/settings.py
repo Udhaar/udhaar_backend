7e9e50009d92f8cdce1f9bdec8aa6abfccadaaaa
Shailesh Aanand <anaandshailu@gmail.com>
2021-07-27 10:56:12 +0530

    Disable ssl for db

--- src/udhaar_backend/settings.py
+++ src/udhaar_backend/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+        default=DATABASE_URL, conn_max_age=500, ssl_require=False)
     DATABASES['default'].update(db_from_env)
 
 # Password validation
